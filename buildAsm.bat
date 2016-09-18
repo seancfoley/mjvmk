e6ab8e888609c393fa4a7201c010c08f772e41c4
sfoley <seancfoley@gmail.com>
2016-09-17 23:26:12 -0400

    update asm build

--- buildAsm.bat
+++ buildAsm.bat
@@ -1,3 +1,15 @@
-"c:\program files\nasm\nasmw.exe" -f bin -o lib\out\x86\psBoot.bin lib\src\ps\x86\psBoot.asm
-"c:\program files\win98ddk\bin\win98\ml.exe" /c /coff /Fllib\out\x86\psCpu.lst /Sa /Sc /Folib\out\x86\psCpu.obj lib\src\ps\x86\psCpu.asm
-"c:\program files\win98ddk\bin\win98\ml.exe" /c /coff /Fllib\out\x86\psEntry.lst /Sa /Sc /Folib\out\x86\psEntry.obj lib\src\ps\x86\psEntry.asm
+SET NASM_DIR=C:\Program Files (x86)\nasm-2.12.02-win32\nasm-2.12.02
+SET VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\
+SET PROJECT_DIR=c:\vstudio\git\mjvmk
+
+cd %NASM_DIR%
+
+nasm -f bin -o %PROJECT_DIR%\lib\out\x86\psBoot.bin %PROJECT_DIR%\lib\src\ps\x86\psBoot.asm
+
+cd %VCINSTALLDIR%bin
+
+ml /c /coff "/Fl%PROJECT_DIR%\lib\out\x86\psCpu.lst" /Sa /Fo%PROJECT_DIR%\lib\out\x86\psCpu.obj %PROJECT_DIR%\lib\src\ps\x86\psCpu.asm
+
+ml /c /coff /Fl%PROJECT_DIR%\lib\out\x86\psEntry.lst /Sa /Fo%PROJECT_DIR%\lib\out\x86\psEntry.obj %PROJECT_DIR%\lib\src\ps\x86\psEntry.asm
+
+
